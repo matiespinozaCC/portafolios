22b53052d5f3758d4090162089de5fbf0216121c
matiespinozaCC <131064240+matiespinozaCC@users.noreply.github.com>
2024-01-14 12:45:13 -0300

    portafolio

---
+++ archivos/js/code.js
@@ -0,0 +1,3 @@
+window.onload = function(){
+    alert("Actualmente me encuentro trabajando en detalles del sitio web por lo cual puede presentar errores en modo dispositivo movil. Muchas gracias!")
+}
